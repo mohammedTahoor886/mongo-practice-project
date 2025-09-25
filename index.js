40cf11aceda6b2a336346be473f355d126d71c19
mohammedTahoor886 <mohammedtahoor886@gmail.com>
2025-09-25 15:50:30 +0530

    mongodb

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let path=require('path');
 const mongoose = require('mongoose');
 const Chat=require('./models/chat.js');
 const methodOverride=require("method-override");
+const expressError= require('./ExpressError.js');
+const {measureMemory} = require('vm');
 
 main().then(res =>{
     console.log("connection success");
@@ -25,15 +27,34 @@ app.get("/",(req,res)=>{
 })
 //index to show or display users
 app.get("/chats", async (req, res) => {
-    let chats= await Chat.find();
-    res.render("index.ejs",{chats});
+    try{
+        let chats= await Chat.find();
+        res.render("index.ejs",{chats});
+    }catch(err){
+        next(err);
+    }
 })
+function ayncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=>next(err));
+    }
+}
+//show cahts
+app.get("/chats/:id/show",ayncWrap (async(req,res,next)=>{
+        let{id}=req.params;
+        let chat =await Chat.findById(id);
+        if(!chat){
+            return next(new expressError(404,"chat not found"));
+        }
+        res.render("show.ejs",{chat})  
+}));
 // create new users
 app.get("/chats/new",(req,res)=>{
     res.render("new.ejs");
 })
 //post the msg
-app.post("/chats",(req,res)=>{
+app.post("/chats",ayncWrap(async(req,res,next)=>{
+  
     let{from,msg,to}=req.body;
     const newChat=new Chat({
         from:from,
@@ -41,34 +62,59 @@ app.post("/chats",(req,res)=>{
         to:to,
         created_by:new Date()
     })
-    newChat.save()
-    .then(res=>{
-        console.log("saved")
-    }).catch(err=>{
-        console.log(err)
-    })
-    res.redirect("/chats");
-})
+    await newChat.save()
+    res.redirect("/chats");    
+}));
+
 //edit the msg
 app.get("/chats/:id/edit",async(req,res)=>{
-    let {id}=req.params;
-    let chat=await Chat.findById(id);
-    res.render("edit.ejs",{chat})
+    try {
+        let {id}=req.params;
+        let chat=await Chat.findById(id);
+        res.render("edit.ejs",{chat})
+    } catch (err) {
+        next(err);
+    }
 })
 
-app.patch("/chats/:id",async(req,res)=>{
-    let{id}=req.params;
-    let {msg:newmsg}=req.body;
-    let newChat=await Chat.findByIdAndUpdate(id,{msg:newmsg},{runValidators:true,new:true})
-    console.log(newChat)
-    res.redirect("/chats")
-})
+app.put("/chats/:id",ayncWrap(async(req,res)=>{
+        let{id}=req.params;
+        let {msg:newmsg}=req.body;
+        let newChat=await Chat.findByIdAndUpdate(id,{msg:newmsg},{runValidators:true,new:true})
+        console.log(newChat)
+        res.redirect("/chats");  
+}));
 //destroy the msg
-app.delete("/chats/:id",async(req,res)=>{
-    let{id}=req.params;
-    let chat= await Chat.findByIdAndDelete(id);
-    console.log(chat);
-    res.redirect("/chats");
+app.delete("/chats/:id",ayncWrap(async(req,res)=>{
+        let{id}=req.params;
+        let chat= await Chat.findByIdAndDelete(id);
+        console.log(chat);
+        res.redirect("/chats");
+}));
+const handleValidation=(err)=>{
+    console.log("This a ValidationError try follow rules");
+    console.dir(err.message);
+    return err;
+}
+const handleCast=(err)=>{
+    console.log("This a CastError try to follow rule");
+    console.dir(err.message);
+    return err;
+}
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name == "ValidationError"){
+        err=handleValidation(err); 
+    }
+    else if(err.name === "CastError"){
+        err=handleCast(err);
+    }
+    next(err);
+})
+//Error Handling Middleware
+app.use((err,req,res,next)=>{
+    let{status=500,message="something error occured"}=err;
+    res.status(status).send(message);
 })
 app.listen("8080",()=>{
     console.log("listening port 8080")
